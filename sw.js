10e7196861cd26037ba629eda486ef7cb2232a95
zzj960 <zzj960@users.noreply.github.com>
2019-06-08 18:56:48 +0800

    Site updated: 2019-06-08 18:56:46

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
 
 'use strict';
 
-var precacheConfig = [["/about/index.html","8fd7d64803da993cf1564a0f49a6731c"],["/archives/index.html","ea76f6d508cd2fb56ff88eaf9194d3b6"],["/bundle.js","c5399921b240b4013f0f9f4f27dd1aad"],["/categories/index.html","215999ceb034a2cdbf0b252ea59e5ef6"],["/images/favicon-16x16-next.ico","c49ba8c36a5774b356b5eb1da436ec11"],["/index.html","23b4dca1e3cf1cc5eb8b9fd7e139a94e"],["/search.xml","639d4fd0eed26e45a3be813fe20d149f"],["/tags/index.html","b384bf8f18c3e2aec55f0b8c110a3e89"],["/uploads/red.jpg","05b6fa256639ed6b757a7adda457680a"]];
+var precacheConfig = [["/about/index.html","52f0cda4ce2a75ef3148442e51d0ca0b"],["/archives/index.html","d6b249ef43c873b90508e93cae481659"],["/bundle.js","c5399921b240b4013f0f9f4f27dd1aad"],["/categories/index.html","b7891a02f0bbe909e35065c45725266a"],["/images/favicon-16x16-next.ico","c49ba8c36a5774b356b5eb1da436ec11"],["/index.html","f4eaa40b8218e858d44cc686d0bae867"],["/search.xml","639d4fd0eed26e45a3be813fe20d149f"],["/tags/index.html","f6b90ac500d3379c4fbcbf7b9c8bf206"],["/uploads/red.jpg","05b6fa256639ed6b757a7adda457680a"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 var firstRegister = 1; // 默认1是首次安装SW， 0是SW更新
 
